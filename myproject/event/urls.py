36ac1f25e556de4ecbc7236976de36556d6ef778
winningJP <github@winning.moe>
2024-12-11 10:38:25 +0900

    イベント詳細、編集、削除機能を追加し、スタイルを改善

--- myproject/event/urls.py
+++ myproject/event/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='event_index'),
     path('create/', views.create_event, name='create_event'),
+    path('<int:id>/', views.event_detail, name='event_detail'),
+    path('<int:id>/edit/', views.edit_event, name='edit_event'),
+    path('<int:id>/delete/', views.delete_event, name='delete_event'),
 ]
--- myproject/event/views.py
+++ myproject/event/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Event
 from .forms import EventForm
 
@@ -16,7 +16,29 @@ def create_event(request):
         form = EventForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('event_index')  # 名前付きURLにリダイレクト
+            return redirect('event_index')
     else:
         form = EventForm()
     return render(request, 'event/create.html', {'form': form})
+
+def event_detail(request, id):
+    event = get_object_or_404(Event, id=id)
+    return render(request, 'event/detail.html', {'event': event})
+
+def edit_event(request, id):
+    event = get_object_or_404(Event, id=id)
+    if request.method == 'POST':
+        form = EventForm(request.POST, instance=event)
+        if form.is_valid():
+            form.save()
+            return redirect('event_detail', id=event.id)
+    else:
+        form = EventForm(instance=event)
+    return render(request, 'event/edit.html', {'form': form, 'event': event})
+
+def delete_event(request, id):
+    event = get_object_or_404(Event, id=id)
+    if request.method == 'POST':
+        event.delete()
+        return redirect('event_index')
+    return render(request, 'event/delete_confirm.html', {'event': event})
